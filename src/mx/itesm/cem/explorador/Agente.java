9a9893dda8f8d2a7cfe3d8d43972fe7f80c84fc5
edvolution <edvolution@b6f6456c-852d-11dd-aace-ddc5e87cd236>
2008-09-24 04:21:31 +0000

    Agente y ResultadoCaminar corregidas y comentadas

--- src/mx/itesm/cem/explorador/Agente.java
+++ src/mx/itesm/cem/explorador/Agente.java
@@ -1,7 +1,7 @@
 package mx.itesm.cem.explorador;
 
-
 public class Agente {
+	
 	private Posicion posicion;
 	private int capacidad;
 	private int cargaActual;
@@ -16,18 +16,11 @@ public class Agente {
 	public static final int DIAG_INF_DER = 0;
 	public static final int DIAG_INF_IZQ = 1;
 
-	public Agente(){
-		
-
-	}
 	public Agente(String id){
 		this.setId(id);
 		this.setPosicion(Tablero.posicionNave);
-		this.setCapacidad((int) Math.random()* ((Tablero.totalPiedras/4)+1));
+		this.setCapacidad((int)(Math.random()* ((Tablero.totalPiedras/4)+1)));
 		this.setCargaActual(0);
-//		this.resultado.setExito(false);
-//		this.resultado.setOcupacion((Nave)(Tablero.matriz[Tablero.posicionNave.getX()][Tablero.posicionNave.getY()]));
-//		this.resultado.setPosicion(Tablero.posicionNave);
 	}
 
 	public Posicion getPosicion() {
@@ -60,6 +53,74 @@ public class Agente {
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public ResultadoCaminar getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(Posicion nuevaPosicion, boolean exito, String ocupacion) {
+		this.resultado.setPosicion(nuevaPosicion);
+		this.resultado.setExito(exito);
+		this.resultado.setOcupacion(ocupacion);
+	}
+
+/**
+ * Metodo principal que controla el comportamiento del agente
+ * recibe un arreglo con las capas, el indice de cada elemento
+ * del arreglo representa el orden de la capa.
+ *  
+ * @param capas Arreglo que contiene las capas en orden de importancia
+ * 
+ */
+	public void actuar(int[] capas){
+		int i =0;
+		boolean exito = false;
+	
+		while (Tablero.piedrasNave != Tablero.totalPiedras) {
+	
+			switch (capas[i]) {
+			case 1:
+				if (this.resultado.getOcupacion() != "-") { //Si la casilla a la que quieres moverte esta ocupada
+					exito = this.evitarObstaculo();
+				}
+				break;
+			case 2:
+				if (this.cargaActual != 0) { //Si lleva piedras su prioridad es ir a la nave
+					/*TODO: Falta usar dejarPiedras*/
+					exito = this.regresarANave();
+				}
+				break;
+			case 3:
+				if (this.resultado.getOcupacion().startsWith("M")) { //Si intentaste moverte a una casilla que tiene un monticulo
+					
+					Monticulo monticulo = (Monticulo)(Tablero.obtenerElementoConId(this.resultado.getOcupacion()));
+					exito = this.cargar(monticulo);
+				}
+				break;
+			case 4:
+				this.explorar();
+				break;
+			default:
+				break;
+			}
+			if (exito) { //Si una capa se ejecuto correctamente, empezar de nuevo a buscar que capa se cumple ahora
+				i = 0;
+			} else {
+				i++; //De lo contrario, intentar con la siguiente capa
+			}
+		}
+	}
+
+/**
+ * Metodo que permite que el agente camine
+ * para posteriormente analizar si pudo moverse o no,
+ * esto le permite explorar su entorno.
+ * @return
+ */
+	public boolean explorar(){
+		this.caminar();
+		return this.resultado.isExito();
 	}
 
 	/* Los movimientos correspoden a los siguientes numeros:
@@ -130,18 +191,15 @@ public class Agente {
 			}
 			break;
 		default:
-			casillaAEvaluar = "-";
-			System.out.println(movimiento);
-			System.out.println("Vali pito! $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+			casillaAEvaluar = null;
 			break;
 		}
 
 		if(casillaAEvaluar == "-"){
-			this.setPosicion(nuevaPosicion);
-			System.out.println("ESTOY AHORA EN [" + this.getPosicion().getI() + ", " + this.getPosicion().getJ() +"]");
-//			casillaAEvaluar = this;
-//			Tablero.matriz[i][j] = "-";
-			this.setResultado(nuevaPosicion, true, null);
+			this.setPosicion(nuevaPosicion); //Actualizamos la posicion del agente
+			Tablero.matriz[nuevaPosicion.getI()][nuevaPosicion.getJ()] = this.getId(); //Colocamos al agente en el tablero, en su nueva posicion
+			Tablero.matriz[i][j] = "-"; //Liberamos la posicion anterior
+			this.setResultado(nuevaPosicion, true, "-");
 
 		}else{
 			this.setResultado(nuevaPosicion, false, casillaAEvaluar);
@@ -149,44 +207,210 @@ public class Agente {
 		return this.resultado;
 	}
 
-	public boolean explorar(){
-		this.caminar();
-		return this.resultado.isExito();
-	}
-
-	public boolean cargar(Monticulo monticulo){
-		int cupo = this.getCapacidad() - this.getCargaActual();
-		if(cupo == 0 || monticulo.getPiedras()==0){
-			return false;
+/**
+ * Metodo sobrecargado que permite que la accion
+ * caminar ya no sea aleatoria, de esta manera 
+ * es posible que el agente se mueva en una direccion
+ * determinada, se usa en regresarANave() 
+ * 
+ * @param movimiento
+ * @return
+ */
+		
+	public ResultadoCaminar caminar(int movimiento){
+		int i = this.posicion.getI();
+		int j = this.posicion.getJ();
+	
+		String casillaAEvaluar = null;
+		Posicion nuevaPosicion = null;
+	
+		switch (movimiento) {
+		case DIAG_INF_DER:
+			if (i<Tablero.CASILLAS && j<Tablero.CASILLAS){
+				nuevaPosicion =new Posicion(i+1,j+1);
+				casillaAEvaluar = Tablero.matriz[i+1][j+1];
+				break;
+			}
+		case DIAG_INF_IZQ:
+			if(i<Tablero.CASILLAS && j>0){
+				nuevaPosicion = new Posicion(i+1,j-1);
+				casillaAEvaluar = Tablero.matriz[i+1][j-1];
+				break;
+			}
+		case ABAJO:
+			if(i<Tablero.CASILLAS){
+				nuevaPosicion = new Posicion(i+1,j);
+				casillaAEvaluar = Tablero.matriz[i+1][j];
+				break;
+			}
+		case DIAG_SUP_DER:
+			if(i>0 && j<Tablero.CASILLAS){
+				nuevaPosicion = new Posicion(i-1,j+1);
+				casillaAEvaluar = Tablero.matriz[i-1][j+1];
+				break;
+			}
+		case DIAG_SUP_IZQ:
+			if(i>0 && j>0){
+				nuevaPosicion = new Posicion(i-1,j-1);
+				casillaAEvaluar = Tablero.matriz[i-1][j-1];
+				break;
+			}
+		case ARRIBA:
+			if(i>0){
+				nuevaPosicion = new Posicion(i-1,j);
+				casillaAEvaluar = Tablero.matriz[i-1][j];
+				break;
+			}
+		case DERECHA:
+			if(j < Tablero.CASILLAS ){
+	
+				nuevaPosicion = new Posicion(i,j+1);
+				casillaAEvaluar = Tablero.matriz[i][j+1];
+				break;
+			}
+	
+		case IZQUIERDA:
+			if(j>0){
+				nuevaPosicion = new Posicion(i,j-1);
+				casillaAEvaluar = Tablero.matriz[i][j-1];
+				break;
+			}
+		default:
+			/*Nunca deberia llegar aqui, pues significaria que la validacion para no salirse del tablero fallo*/
+			casillaAEvaluar = null; 
 		}
-		monticulo.setPiedras(monticulo.getPiedras() - cupo);
-		this.setCargaActual(this.getCargaActual() + cupo);
-		return true;
+	
+		if(casillaAEvaluar == "-"){
+			this.setPosicion(nuevaPosicion);
+			Tablero.matriz[nuevaPosicion.getI()][nuevaPosicion.getJ()] = this.getId();
+			Tablero.matriz[i][j] = "-";
+			this.setResultado(nuevaPosicion, true, "-");
+	
+		}else{
+			this.setResultado(nuevaPosicion, false, casillaAEvaluar);
+		}
+		return this.resultado;
 	}
 
-	/*TODO*/
-	public boolean regresarANave(){
-		int xRelativa = Tablero.posicionNave.getI() - this.getPosicion().getI();
-		int yRelativa = Tablero.posicionNave.getJ() - this.getPosicion().getJ();
+	/**
+	 * Metodo que permite evadir obstaculos
+	 * a partir de intentar moverse en todas direcciones
+	 * a su alrededor hasta que encuentra una casilla
+	 * vacia a donde podra moverse
+	 * 
+	 * @return
+	 */
+	public boolean evitarObstaculo(){
+		int i = this.posicion.getI();
+		int j = this.posicion.getJ();
+		int movimiento = 0;
+	
+		String casillaAEvaluar = null;
+		Posicion nuevaPosicion = null;
+	
+	
+		while(true){
+			switch (movimiento) {
+			case DIAG_INF_DER:
+				if (i<Tablero.CASILLAS && j<Tablero.CASILLAS){
+					nuevaPosicion =new Posicion(i+1,j+1);
+					casillaAEvaluar = Tablero.matriz[i+1][j+1];
+					break;
+				}
+			case DIAG_INF_IZQ:
+				if(i<Tablero.CASILLAS && j>0){
+					nuevaPosicion = new Posicion(i+1,j-1);
+					casillaAEvaluar = Tablero.matriz[i+1][j-1];
+					break;
+				}
+			case ABAJO:
+				if(i<Tablero.CASILLAS){
+					nuevaPosicion = new Posicion(i+1,j);
+					casillaAEvaluar = Tablero.matriz[i+1][j];
+					break;
+				}
+			case DIAG_SUP_DER:
+				if(i>0 && j<Tablero.CASILLAS){
+					nuevaPosicion = new Posicion(i-1,j+1);
+					casillaAEvaluar = Tablero.matriz[i-1][j+1];
+					break;
+				}
+			case DIAG_SUP_IZQ:
+				if(i>0 && j>0){
+					nuevaPosicion = new Posicion(i-1,j-1);
+					casillaAEvaluar = Tablero.matriz[i-1][j-1];
+					break;
+				}
+			case ARRIBA:
+				if(i>0){
+					nuevaPosicion = new Posicion(i-1,j);
+					casillaAEvaluar = Tablero.matriz[i-1][j];
+					break;
+				}
+			case DERECHA:
+				if(j < Tablero.CASILLAS ){
+	
+					nuevaPosicion = new Posicion(i,j+1);
+					casillaAEvaluar = Tablero.matriz[i][j+1];
+					break;
+				}
+	
+			case IZQUIERDA:
+				if(j>0){
+					nuevaPosicion = new Posicion(i,j-1);
+					casillaAEvaluar = Tablero.matriz[i][j-1];
+					break;
+				}
+			default:
+				this.setResultado(nuevaPosicion, false, casillaAEvaluar);
+				return false;
+			}
+	
+			if(casillaAEvaluar == "-"){
+				this.setPosicion(nuevaPosicion);
+				Tablero.matriz[nuevaPosicion.getI()][nuevaPosicion.getJ()] = this.getId();
+				Tablero.matriz[i][j] = "-";
+				this.setResultado(nuevaPosicion, true, "-");
+				
+				return true;
+				
+			}else{
+				movimiento++;
+			}
+		}
+	
+	}
 
-		if(xRelativa == 0){
-			if(yRelativa<0){
+	/**
+	 * Metodo que permite que el agente regrese a la nave
+	 * a traves de ir calculando en que direccion relativa 
+	 * a el se encuentra la nave e irse acercando cada vez mas
+	 * a ella
+	 * 
+	 * @return
+	 */
+	public boolean regresarANave(){
+		int iRelativa = Tablero.posicionNave.getI() - this.getPosicion().getI();
+		int jRelativa = Tablero.posicionNave.getJ() - this.getPosicion().getJ();
+	
+		if(iRelativa == 0){
+			if(jRelativa<0){
 				this.caminar(DERECHA);
 			}else{
 				this.caminar(IZQUIERDA);
 			}
 		}
-
-		if(yRelativa == 0){
-			if(xRelativa<0){
+	
+		if(jRelativa == 0){
+			if(iRelativa<0){
 				this.caminar(ABAJO);
 			}else{
 				this.caminar(ARRIBA);
 			}
 		}
-
-		if(xRelativa < 0){
-			if(yRelativa<0){
+	
+		if(iRelativa < 0){
+			if(jRelativa<0){
 				//1,2,4
 				if(this.caminar(DIAG_SUP_IZQ).isExito()){
 					return true;
@@ -195,7 +419,7 @@ public class Agente {
 				}else{
 					return this.caminar(ARRIBA).isExito();
 				}
-
+	
 			}else{
 				//2,3,5
 				if(this.caminar(DIAG_SUP_DER).isExito()){
@@ -207,7 +431,7 @@ public class Agente {
 				}
 			}
 		}else{
-			if(yRelativa<0){
+			if(jRelativa<0){
 				if(this.caminar(DIAG_INF_IZQ).isExito()){
 					return true;
 				}else if(this.caminar(IZQUIERDA).isExito()){
@@ -227,84 +451,14 @@ public class Agente {
 		}
 	}
 
-	public boolean evitarObstaculo(){
-		int x = this.posicion.getI();
-		int y = this.posicion.getJ();
-		int movimiento = 0;
-
-		Object casillaAEvaluar = null;
-		Posicion nuevaPosicion = null;
-
-
-		while(true){
-			switch (movimiento) {
-			case 0:
-				if (x<Tablero.CASILLAS && y<Tablero.CASILLAS){
-					nuevaPosicion =new Posicion(x+1,y+1);
-					casillaAEvaluar = Tablero.matriz[x+1][y+1];
-					break;
-				}
-			case 1:
-				if(x<Tablero.CASILLAS && y>0){
-					nuevaPosicion = new Posicion(x+1,y-1);
-					casillaAEvaluar = Tablero.matriz[x+1][y-1];
-					break;
-				}
-			case 2:
-				if(x<Tablero.CASILLAS){
-					nuevaPosicion = new Posicion(x+1,y);
-					casillaAEvaluar = Tablero.matriz[x+1][y];
-					break;
-				}
-			case 3:
-				if(x>0 && y<Tablero.CASILLAS){
-					nuevaPosicion = new Posicion(x-1,y+1);
-					casillaAEvaluar = Tablero.matriz[x-1][y+1];
-					break;
-				}
-			case 4:
-				if(x>0 && y>0){
-					nuevaPosicion = new Posicion(x-1,y-1);
-					casillaAEvaluar = Tablero.matriz[x-1][y-1];
-					break;
-				}
-			case 5:
-				if(x>0){
-					nuevaPosicion = new Posicion(x-1,y);
-					casillaAEvaluar = Tablero.matriz[x-1][y];
-					break;
-				}
-			case DERECHA:
-				if(y < Tablero.CASILLAS ){
-
-					nuevaPosicion = new Posicion(x,y+1);
-					casillaAEvaluar = Tablero.matriz[x][y+1];
-					break;
-				}
-
-			case 7:
-				if(y>0){
-					nuevaPosicion = new Posicion(x,y-1);
-					casillaAEvaluar = Tablero.matriz[x][y-1];
-					break;
-				}
-			default:
-				this.setResultado(nuevaPosicion, false, casillaAEvaluar);
-				return false;
-			}
-
-			if(casillaAEvaluar == null){
-				this.setPosicion(nuevaPosicion);
-				casillaAEvaluar = this;
-				Tablero.matriz[x][y] = null;
-				this.setResultado(nuevaPosicion, true, null);
-				return true;
-
-			}else{
-				movimiento++;
-			}
+	public boolean cargar(Monticulo monticulo){
+		int cupo = this.getCapacidad() - this.getCargaActual();
+		if(cupo == 0 || monticulo.getPiedras()==0){
+			return false;
 		}
-
+		monticulo.setPiedras(monticulo.getPiedras() - cupo);
+		this.setCargaActual(this.getCargaActual() + cupo);
+		return true;
 	}
 
 	public boolean dejarPiedras(){
@@ -312,127 +466,6 @@ public class Agente {
 		this.setCargaActual(0);
 		return true;
 	}
-
-	public void setResultado(Posicion nuevaPosicion, boolean exito, Object ocupacion) {
-		this.resultado.setPosicion(nuevaPosicion);
-		this.resultado.setExito(exito);
-		this.resultado.setOcupacion(ocupacion);
-	}
-
-	public void actuar(int[] capas){
-		int i =0;
-		boolean exito = false;
-
-		while (Tablero.piedrasNave != Tablero.totalPiedras) {
-
-			switch (capas[i]) {
-			case 1:
-				if (this.resultado.getOcupacion() != null) {
-					exito = this.evitarObstaculo();
-				}
-				break;
-			case 2:
-				if (this.cargaActual != 0) {
-					exito = regresarANave();
-				}
-				break;
-			case 3:
-				if (this.resultado.getOcupacion() instanceof Monticulo) {
-					exito = this.cargar((Monticulo)this.resultado.getOcupacion());
-				}
-				break;
-			case 4:
-				this.explorar();
-				break;
-			default:
-				break;
-			}
-			if (exito) {
-				i = 0;
-			} else {
-				i++;
-			}
-		}
-	}
-
-	public ResultadoCaminar getResultado() {
-		return resultado;
-	}
-
-	public ResultadoCaminar caminar(int movimiento){
-		int i = this.posicion.getI();
-		int j = this.posicion.getJ();
-
-		Object casillaAEvaluar = null;
-		Posicion nuevaPosicion = null;
-
-		switch (movimiento) {
-		case 0:
-			if (i<Tablero.CASILLAS && j<Tablero.CASILLAS){
-				nuevaPosicion =new Posicion(i+1,j+1);
-				casillaAEvaluar = Tablero.matriz[i+1][j+1];
-				break;
-			}
-		case 1:
-			if(i<Tablero.CASILLAS && j>0){
-				nuevaPosicion = new Posicion(i+1,j-1);
-				casillaAEvaluar = Tablero.matriz[i+1][j-1];
-				break;
-			}
-		case 2:
-			if(i<Tablero.CASILLAS){
-				nuevaPosicion = new Posicion(i+1,j);
-				casillaAEvaluar = Tablero.matriz[i+1][j];
-				break;
-			}
-		case 3:
-			if(i>0 && j<Tablero.CASILLAS){
-				nuevaPosicion = new Posicion(i-1,j+1);
-				casillaAEvaluar = Tablero.matriz[i-1][j+1];
-				break;
-			}
-		case 4:
-			if(i>0 && j>0){
-				nuevaPosicion = new Posicion(i-1,j-1);
-				casillaAEvaluar = Tablero.matriz[i-1][j-1];
-				break;
-			}
-		case 5:
-			if(i>0){
-				nuevaPosicion = new Posicion(i-1,j);
-				casillaAEvaluar = Tablero.matriz[i-1][j];
-				break;
-			}
-		case 6:
-			if(j < Tablero.CASILLAS ){
-
-				nuevaPosicion = new Posicion(i,j+1);
-				casillaAEvaluar = Tablero.matriz[i][j+1];
-				break;
-			}
-
-		case 7:
-			if(j>0){
-				nuevaPosicion = new Posicion(i,j-1);
-				casillaAEvaluar = Tablero.matriz[i][j-1];
-				break;
-			}
-		default:
-			casillaAEvaluar = null;
-		}
-
-		if(casillaAEvaluar == null){
-			this.setPosicion(nuevaPosicion);
-			casillaAEvaluar = this;
-			Tablero.matriz[i][j] = null;
-			this.setResultado(nuevaPosicion, true, null);
-
-		}else{
-			this.setResultado(nuevaPosicion, false, casillaAEvaluar);
-		}
-		return this.resultado;
-	}
-
 
 	/* Se implementara en la segunda fase **/
 	public boolean dejarMoronas(){
