f7f04b858c46863580e51be440595d29f364e1d1
migue030 <migue030@b6f6456c-852d-11dd-aace-ddc5e87cd236>
2008-11-30 21:21:32 +0000

    se agregó la clase agente explorador, modifico el modificador de acceso de las listas del cargador para ser vistas desde el explorador y se agrego la propiedad de contrato abierto en montiuclo para saber si el contrato aun esta disponible

--- trunk/src/mx/itesm/cem/explorador/Monticulo.java
+++ trunk/src/mx/itesm/cem/explorador/Monticulo.java
@@ -6,7 +6,8 @@ public class Monticulo {
 	private Posicion posicion;
 	private String id;
 	int piedras = 0;
-	
+	private boolean contratoAbierto = false;
+
 	public Monticulo(String id, Posicion pos){
 		this.setId(id);
 		this.setPosicion(pos);
@@ -20,19 +21,27 @@ public class Monticulo {
 	public void setPosicion(Posicion posicion) {
 		this.posicion = posicion;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public int getPiedras() {
 		return piedras;
 	}
 	public void setPiedras(int piedras) {
 		this.piedras = piedras;
+	}
+
+	public boolean isContratoAbierto() {
+		return contratoAbierto;
+	}
+
+	public void setContratoAbierto(boolean contratoAbierto) {
+		this.contratoAbierto = contratoAbierto;
 	}
 
 }
